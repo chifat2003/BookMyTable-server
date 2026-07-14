88d25db55921e97d94414766b0ac59be9ad2573b
chifat2003 <nmchifat2004@gmail.com>
2026-07-15 01:34:10 +0600

    deployment fix

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,12 @@ import { getDB } from './config/db';
 
 const app: Express = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
